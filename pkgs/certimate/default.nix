e4a1e0189b8fdd7b385c505adf62338a78bd4511
Allen You <i@allenyou.wang>
2026-03-11 21:30:14 +0800

    fix(certimate): 修复src sha256

--- pkgs/certimate/default.nix
+++ pkgs/certimate/default.nix
@@ -11,7 +11,7 @@ buildGoModule {
     repo = "certimate";
     rev = "v0.4.18";
     fetchSubmodules = false;
-    sha256 = "sha256-y7SD+czD/jK/m0LbFq7qGjwJgBIXfTNrdsA3pzgD2xE=";
+    sha256 = "sha256-xaH4JYD+EuKucFUH5XhOXbp+A8xNimsXKXPXj5C9w8k=";
   });
   vendorHash = "sha256-ZrwFUZDTbJx5qvloVOa5qK1ykKNkUn1hjfz0xf+8sWk=";
 }

f9e4cb7111fabe834d6eedd2d3a5e6e6cf1a18d6
Allen You <i@allenyou.wang>
2026-03-11 21:27:10 +0800

    pkgs(certimate): 更换HASH

--- pkgs/certimate/default.nix
+++ pkgs/certimate/default.nix
@@ -11,7 +11,7 @@ buildGoModule {
     repo = "certimate";
     rev = "v0.4.18";
     fetchSubmodules = false;
-    sha256 = "sha256-3333333333333333333333333333333333333333333333333333333333333333";
+    sha256 = "sha256-y7SD+czD/jK/m0LbFq7qGjwJgBIXfTNrdsA3pzgD2xE=";
   });
-  vendorSha256 = "sha256-3333333333333333333333333333333333333333333333333333333333333333+8sWk=";
+  vendorSha256 = "sha256-ZrwFUZDTbJx5qvloVOa5qK1ykKNkUn1hjfz0xf+8sWk=";
 }

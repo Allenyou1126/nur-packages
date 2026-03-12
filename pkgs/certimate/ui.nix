4ada2933bf48d6dae3fafa959c8fe2182c8b96e9
Allen You <i@allenyou.wang>
2026-03-12 18:08:04 +0800

    fix(certimate): 修复UI打包内存不足

--- pkgs/certimate/ui.nix
+++ pkgs/certimate/ui.nix
@@ -14,9 +14,9 @@ buildNpmPackage rec {
     fetchSubmodules = false;
     sha256 = "sha256-xaH4JYD+EuKucFUH5XhOXbp+A8xNimsXKXPXj5C9w8k=";
   });
-  npmBuildFlags = [
-    "--max-old-space-size=4096"
-  ];
+  env = {
+    NODE_OPTIONS = "--max-old-space-size=4096";
+  };
   npmDepsHash = "sha256-Lxcz0ztIn4vH+Q4WFcCqlRJOklUyyC2FvVRUqd8Da5I=";
   sourceRoot = "${src.name}/ui";
   nodejs = pkgs.nodejs_24;
